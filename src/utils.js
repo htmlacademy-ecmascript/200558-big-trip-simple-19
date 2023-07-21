e441c89a4912d0d4aa4a07933dc0895cc8995c20
Дмитрий Головин <dmitriy.golovin.2001.03@mail.ru>
2023-07-21 17:21:25 +0300

    задание 5.11

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,21 @@
 export const sort = {
-    min: (object, property) => object.sort((a, b) => a[property] - b[property]),
+  min: (object, property) => object.sort((a, b) => a[property] - b[property]),
 };
 const RenderPosition = {
-    BEFOREBEGIN: 'beforebegin',
-    AFTERBEGIN: 'afterbegin',
-    BEFOREEND: 'beforeend',
-    AFTEREND: 'afterend',
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
 };
 
 export function createElement(template) {
-    const newElement = document.createElement('div');
-    newElement.innerHTML = template;
-    return newElement.firstElementChild;
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+  return newElement.firstElementChild;
 }
 export function render(component, container, place = RenderPosition.BEFOREEND) {
-    container.insertAdjacentElement(place, component.element);
+  container.insertAdjacentElement(place, component.element);
 }
 export function replaceElement(newElement, oldElement) {
-    oldElement.parentNode.replaceChild(newElement, oldElement);
+  oldElement.parentNode.replaceChild(newElement, oldElement);
 }
