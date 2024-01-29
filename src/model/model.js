71ca1b3ef0be399a8e6ab6640515d1993e8b70a6
Дмитрий Головин <dmitriy.golovin.2001.03@mail.ru>
2024-01-29 15:24:33 +0300

    задание 8.30

--- src/model/model.js
+++ src/model/model.js
@@ -1,6 +1,5 @@
 import ApiService from '.././api-service.js';
 import Observable from '../framework/observable.js';
-// const observable = new Observable();
 const UPDATE_TYPE = {
   INIT: 'INIT',
   REMOVE: 'REMOVE',
@@ -18,23 +17,7 @@ class Model extends Observable {
   }
 
   async init() {
-    //let i = 0;
-    //const dataParameters = [[api.getPoints(), 'points'], [api.getDestinations(), 'destinations'], [api.getOffers(), 'offers']];
     const dataParameters = [api.getPoints(), api.getDestinations(), api.getOffers()];
-
-    // for (const [promis, nameData] of dataParameters) {
-    //   promis.then((data) => {
-    //     this[nameData] = adaptClient(data);
-    //     i++;
-    //     if (i === 3) {
-    //       this._notify(UPDATE_TYPE.INIT);
-    //     }
-    //   }).catch(() => {
-    //     errrorLog();
-    //     errrorLog = null;
-    //   });
-    //  }
-
     Promise.all(dataParameters).then(([points, destinations, offers]) => {
       this.points = adaptClient(points);
       this.destinations = adaptClient(destinations);
